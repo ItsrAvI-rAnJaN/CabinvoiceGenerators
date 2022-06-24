b75a24e0c2935fba93da926901d53c21de148e1f
Ravi Ranjan <92181941+ravi04ez@users.noreply.github.com>
2022-06-25 01:01:37 +0530

    Wlcm Message to Cab Invoice

---
+++ CabInvoiceGenerators/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CabInvoiceGenerators
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t******* WELCOME TO CAB INVOICE GENERATOR PROGRAM *******");
+            Console.ReadKey();
+        }
+    }
+}
